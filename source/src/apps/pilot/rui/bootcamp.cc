2ce52121f9f24185940edd012d1a6cb589b78ecf
Rui Huang <r1huang@ucsd.edu>
2024-01-30 13:53:31 -0800

    "added packing, minimization"

--- source/src/apps/pilot/rui/bootcamp.cc
+++ source/src/apps/pilot/rui/bootcamp.cc
@@ -14,15 +14,83 @@
 #include <core/import_pose/import_pose.hh>
 #include <utility/pointer/owning_ptr.hh>
 #include <core/pose/Pose.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <protocols/moves/MonteCarlo.hh>
+#include <numeric/random/random.hh>
+#include <numeric/random/uniform.hh>
+#include <protocols/moves/PyMOLMover.hh>
+#include <core/pack/task/PackerTask.hh>
+#include <core/pack/pack_rotamers.hh>
+#include <core/optimization/MinimizerOptions.hh>
+#include <core/optimization/AtomTreeMinimizer.hh>
+#include <core/pack/task/TaskFactory.hh>
+#include <core/kinematics/MoveMap.hh>
 
 int
 main( int argc, char ** argv ) {
 	devel::init( argc, argv );
 	utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
 	if ( filenames.size() > 0 ) {
+        
+        core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1]);
+        
+        core::scoring::ScoreFunctionOP sfxn = core::scoring::get_score_function();
+        core::Real score = sfxn->score( *mypose );
+        
+        std::cout << "score: " << score << std::endl;
+        
 		std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
+        
+        
+        
+        core::Real uniform_random_number = numeric::random::uniform();
+        
+        core::Size N = (mypose->size()) / (mypose->total_residue());
+        //… code here to pick the index of a random residue in the Pose
+        core::Size randres = static_cast< core::Size > ( uniform_random_number * N + 1 );
+        
+
+        protocols::moves::MonteCarlo mc1(*mypose, *sfxn, 1);
+        
+        protocols::moves::PyMOLObserverOP the_observer = protocols::moves::AddPyMOLObserver( *mypose, true, 0 );
+        the_observer->pymol().apply( *mypose);
+         
+        for (int itr = 0; itr < 500; ++itr) {
+            core::Real pert1 = numeric::random::gaussian();
+            //… code here to get a random number
+            core::Real pert2 = numeric::random::gaussian();
+            //… code here to get another random number
+            core::Real orig_phi = mypose->phi( randres );
+            core::Real orig_psi = mypose->psi( randres );
+            mypose->set_phi( randres, orig_phi + pert1 );
+            mypose->set_psi( randres, orig_psi + pert2 );
+            
+            core::pack::task::PackerTaskOP repack_task =
+            core::pack::task::TaskFactory::create_packer_task( *mypose );
+            repack_task->restrict_to_repacking();
+            core::pack::pack_rotamers( *mypose, *sfxn, repack_task );
+            
+            core::kinematics::MoveMap mm;
+            mm.set_bb( true );
+            mm.set_chi( true );
+
+            core::optimization::MinimizerOptions min_opts( "lbfgs_armijo_atol", 0.01, true );
+            core::optimization::AtomTreeMinimizer atm;
+            //run minimizer:
+            atm.run( *mypose, mm, *sfxn, min_opts );
+            
+            core::pose::Pose copy_pose = *mypose;
+            atm.run( copy_pose, mm, *sfxn, min_opts );
+            *mypose = copy_pose;
+
+            mc1.boltzmann(*mypose);
+        }
+        
 	} else {
 		std::cout << "You did not provide a PDB file with the -in::file::s option" << std::endl;
 		return 1;
 	}
 }
+
+// bin/bootcamp.default.macosclangdebug -in:file:s /Users/apple/Downloads/rosetta/testFile/1ubq.pdb -database /Users/apple/Downloads/rosetta/rosetta/database -ignore_unrecognized_res
